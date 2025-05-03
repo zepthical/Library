6fcd021f9362d4a7c05a0e1a5cb45963ddb0fb56
Zepth <taitanerenwearenumber@gmail.com>
2025-05-03 18:25:44 +0700

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,1 +1,16 @@
+local Window = require(script.Window)
+local Tab = require(script.Tab)
 
+local UILibrary = {}
+
+function UILibrary:CreateWindow(title)
+    local window = Window.new(title)
+
+    function window:CreateTab(tabName)
+        return Tab.new(tabName, self.MainFrame)
+    end
+
+    return window
+end
+
+return UILibrary
