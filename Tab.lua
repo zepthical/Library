57ab01fc25e0608d45790fae4b955eeff62ce173
Zepth <taitanerenwearenumber@gmail.com>
2025-05-03 20:27:34 +0700

    Update Tab.lua

--- Tab.lua
+++ Tab.lua
@@ -1,22 +1,20 @@
--- Tab.lua
-local Button = loadstring(game:HttpGet("https://raw.githubusercontent.com/zepthical/Library/main/Library/Button.lua"))()
+local Button = loadstring(game:HttpGet("https://raw.githubusercontent.com/zepthical/Library/main/Library/Elements/Button.lua"))()
 
-return function(tabName, parent)
-    local tab = {}
+return function(config, parent)
+    local self = {}
+    self.__index = self
 
     local label = Instance.new("TextLabel", parent)
-    label.Name = tabName .. "_Tab"
-    label.Text = tabName
+    label.Name = config.Name .. "_Tab"
+    label.Text = config.Name
     label.Size = UDim2.new(1, 0, 0, 30)
     label.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
     label.TextColor3 = Color3.fromRGB(255, 255, 255)
     label.BorderSizePixel = 0
 
-    tab.Frame = label
-
-    function tab:Button(config)
-        return Button(config, label)
+    function self:Button(buttonConfig)
+        return Button(buttonConfig, label)
     end
 
-    return tab
+    return setmetatable(self, self)
 end
