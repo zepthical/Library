441e02d49599583c44bfa198946fd9098d37000a
Zepth <taitanerenwearenumber@gmail.com>
2025-05-03 18:25:03 +0700

    Update Tab.lua

--- Tab.lua
+++ Tab.lua
@@ -1,1 +1,25 @@
+local Button = require(script.Parent.Elements.Button)
 
+local Tab = {}
+Tab.__index = Tab
+
+function Tab.new(title, parentFrame)
+    local self = setmetatable({}, Tab)
+
+    local tabFrame = Instance.new("Frame")
+    tabFrame.Size = UDim2.new(1, 0, 1, 0)
+    tabFrame.BackgroundTransparency = 1
+    tabFrame.Name = title
+    tabFrame.Parent = parentFrame
+
+    self.Frame = tabFrame
+    self.Title = title
+
+    return self
+end
+
+function Tab:Button(options)
+    Button.new(options, self.Frame)
+end
+
+return Tab
